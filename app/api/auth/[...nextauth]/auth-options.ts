855c028ba4ffa4218fc1e3fe472c2a2ea4e713d4
Song Vasconcellos <song.vasconcellos@portoseguro.com.brasil>
2026-03-28 16:16:25 -0300

    feat: primeiro commit da big coxinhas

---
+++ app/api/auth/[...nextauth]/auth-options.ts
@@ -0,0 +1,92 @@
+import { supabase } from "@/app/lib/supabase"
+import { Session } from "next-auth"
+import { JWT } from "next-auth/jwt"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+declare module 'next-auth' {
+  interface Session {
+    user?: {
+      id?: string
+      name?: string
+      email?: string
+      role?: string
+    }
+  }
+}
+interface CustomToken extends JWT {
+  id?: string
+  name?: string
+  email?: string
+  role?: string
+}
+
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: 'Name', type: 'text' },
+        password: { label: 'Senha', type: 'password' },
+      },
+      async authorize(
+        credentials: Record<'email' | 'password', string> | undefined,
+        req: unknown
+      ) {
+        if (!credentials) {
+          throw new Error('Missing credentials')
+        }
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+          email: credentials.email,
+          password: credentials.password,
+        })
+
+        console.log('🔥🔥🔥🔥 Supabase 1:', data)
+        console.log('🔥🔥🔥🔥 Supabase 1:', data?.user?.id)
+        if (error || !data.user) { return null }
+
+        const { data: teste } = await supabase
+          .from('profiles')
+          .select('*')
+          .eq('user_id', data.user.id)
+          .single()
+
+        console.log('🔥🔥🔥🔥 Supabase 2:', teste)
+        if (error || !data.user) return null
+
+        return {
+          id: data.user.id,
+          email: data.user.email,
+          name: teste?.name,
+          role: teste?.role
+        }
+      }
+    })
+  ],
+  session: {
+    strategy: "jwt"
+  },
+  callbacks: {
+    async jwt({ token, user } : { token: CustomToken, user: Session['user'] }) {
+      if (user) {
+        token.id = user.id
+        token.email = user.email
+        token.name = user.name
+        token.role = user.role
+      }
+
+      return token
+    },
+    async session({ session, token } : { session: Session, token: CustomToken }) {
+      session.user = {
+        id: token.id,
+        email: token.email,
+        name: token.name,
+        role: token.role
+      }
+      return session
+    }
+
+  },
+}
---
+++ app/page.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import Image from "next/image";
+import { supabase } from '../app/lib/supabase'
+import { useSession } from "next-auth/react";
+
+
+export default function Home() {
+  const { data: session, status } = useSession()
+    
+
+  if (status === 'loading') {
+    return <p>Carregando...</p>
+  }
+
+  if (!session) {
+    return <p>Você não está logado.</p>
+  }
+  // const { data, error } = await supabase
+  // .from('product')
+  // .select('*')
+
+  // if (error) {
+  //   console.log(error)
+  // }
+
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+       
+  
+      </main>
+    </div>
+  );
+}
